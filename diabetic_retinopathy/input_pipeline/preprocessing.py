bd891d8fc60d94c02a2583746d09bfa333c244ca
st172325 <st172353@stud.uni-stuttgart.de>
2021-11-09 15:16:10 +0100

    added cropping as preprocessing

--- diabetic_retinopathy/input_pipeline/preprocessing.py
+++ diabetic_retinopathy/input_pipeline/preprocessing.py
@@ -9,7 +9,9 @@ def preprocess(image, label, img_height, img_width):
     tf.cast(image, tf.float32) / 255.
 
     # Resize image
+    image = tf.image.crop_to_bounding_box(image, 0, 550, 2848, 2848)
     image = tf.image.resize(image, size=(img_height, img_width))
+
 
     return image, label
 
