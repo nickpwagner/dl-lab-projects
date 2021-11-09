ac8eea45ba368c5dfe354e6371a48b2e9d4b5465
NickWagner <st175644@stud.uni-stuttgart.de>
2021-11-09 15:15:46 +0100

    crop and resize done

--- diabetic_retinopathy/input_pipeline/preprocessing.py
+++ diabetic_retinopathy/input_pipeline/preprocessing.py
@@ -9,7 +9,9 @@ def preprocess(image, label, img_height, img_width):
     tf.cast(image, tf.float32) / 255.
 
     # Resize image
-    image = tf.image.resize(image, size=(img_height, img_width))
+    image = tf.image.crop_to_bounding_box(image, 0, 560, 2848, 2848) # crop center bounding box
+    image = tf.image.resize(image, [img_height, img_width], method=tf.image.ResizeMethod.BILINEAR,preserve_aspect_ratio=False)
+    image = image / 255. # rescale
 
     return image, label
 
