6f63ef06680992e0a6cc5f43cca3b196b1c7afbf
st172353 <st172353>
2021-12-14 22:47:32 +0100

    added f1 score

--- train.py
+++ train.py
@@ -1,8 +1,32 @@
 import tensorflow.keras as keras
+import tensorflow as tf
 import logging
 import wandb
 from wandb.keras import WandbCallback
+import tensorflow_addons as tfa
 
+
+"""
+def recall_m(y_train, y_pred):
+    confm = tf.math.confusion_matrix(tf.math.argmax(y_train, axis=1), tf.math.argmax(y_pred, axis=1))
+    
+    precision = [confm[i][i] / (tf.reduce_sum(confm, axis=1)[i]+ 1e-9) for i in range(tf.shape(confm)[0])]
+    return tf.reduce_mean(precision)
+
+def precision_m(y_train, y_pred):
+    confm = tf.math.confusion_matrix(tf.math.argmax(y_train, axis=1), tf.math.argmax(y_pred, axis=1))
+    recalls = []
+    for i in range(tf.shape(confm)[0]):
+        recalls.append(tf.cast(confm[i][i], tf.float32) / (tf.cast(tf.reduce_sum(confm, axis=0)[i], tf.float32)+ 1e-9) )
+    recall = tf.reduce_mean(recalls)
+    return recall
+
+def f1_m(y_train, y_pred):
+    r = recall_m(y_train, y_pred)
+    p = precision_m(y_train, y_pred)
+
+    return 2*r*p/(r+p+1e-9)
+"""
 
 def train(config, model, ds_train, ds_val): 
     if config.optimizer=="sgd":
@@ -18,7 +42,7 @@ def train(config, model, ds_train, ds_val):
         metrics = ["accuracy"]
     elif config.mode == "multi_class":
         loss = "categorical_crossentropy"
-        metrics = ["accuracy", keras.metrics.CategoricalAccuracy()]
+        metrics = ["accuracy", tfa.metrics.F1Score(5)]    
     elif config.mode == "regression":
         loss = "mean_squared_error"
         metrics = ["mean_squared_error"]
@@ -65,3 +89,7 @@ def train(config, model, ds_train, ds_val):
                 verbose=2,
                 validation_data=ds_val,
                 callbacks=[WandbCallback()])
+
+
+if __name__ == "__main__":
+    pass
