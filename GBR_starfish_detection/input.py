8e43e8d0c747f3725acd992e211fe5d3227ddcc6
st172353 <st172353>
2022-02-04 09:05:18 +0100

    started adapting the input pipeline

--- GBR_starfish_detection/input.py
+++ GBR_starfish_detection/input.py
@@ -3,31 +3,155 @@ import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt 
 import ast
+import os
+
+class DataLoader:
+
+    def __init__(self, config):
+        self.config = config
+        if not self.config.data_partial:
+            self.black_list_files_train = ["00001505_skid-pad.txt", 
+                        "Renningen_08_04_video_second_frame_8.txt", 
+                        "Aidlingen_07_12_video_first_frame_4.txt",
+                        "Aidlingen_07_12_video_first_frame_8.txt",
+                        "upbracing-classes.txt",
+                        "00001833_skid-pad.txt",
+                        "00047.txt",
+                        "Aidlingen_07_12_video_first_frame_7.txt",
+                        "00001465_skid-pad.txt",
+                        "Aidlingen_07_12_video_first_frame_5.txt",
+                        "Aidlingen_07_12_video_first_frame_6.txt",
+                        "213(2).txt",
+                        "Aidlingen_07_12_video_first_frame_3.txt",
+                        "00001464_skid-pad.txt"
+                        ]  
+                        # the labels of this samples are currupt and may not be used
+
+        self.black_list_files_test = []
 
 
-def load(config):
+    def bbox_to_grid(config, bboxes):
+        """
+        read a list of labels and convert it into a grid
+        """
+        y = np.zeros((config.grid_size, config.grid_size, 5))
 
-    # read csv file
-    df = pd.read_csv(config.data_dir + "train.csv")[config.dataset_slice_start:config.dataset_slice_end]  #[20:180]
-
-    print(df.head())
-    print(f"Loading {len(df)} images.")
-
-    # read image names and convert them into the path video_id/img_id
-    img_names = [f"video_{img_name.split('-')[0]}/{img_name.split('-')[1]}.jpg" for img_name in df["image_id"]]
-    # read bounding box locations
-    bboxes = df["annotations"]
-    # translate y string from csv to 7x7x5 grid
-    y = [bbox_to_grid(config, bboxes.iloc[i]) for i in range(len(bboxes))]
-    len_train_ds = int(len(y)*(1 - config.test_split))
-    len_test_ds = int(len(y) * config.test_split)
+        bboxes = ast.literal_eval(bboxes)
+        # calculate grid positions for bbox centers
+        for bbox in bboxes:
+            # absolute coordinates for center 
+            # e.g. x_abs = position + half the width 
+            x_center_abs = bbox["x"] + int(bbox["width"] / 2)
+            y_center_abs = bbox["y"] + int(bbox["height"] / 2)
+            # to which grid cell do those coordinates belong
+            # e.g. x_center_abs: 584 to cell 3
+            i = int(x_center_abs / config.img_width * config.grid_size) # grid cell x
+            j = int(y_center_abs / config.img_height * config.grid_size) # grid cell y
+            if i == config.grid_size:
+                i = i-1
+            if j == config.grid_size:
+                j = j-1
+            # absolute width/height per cell
+            cell_size_x_abs = config.img_width/config.grid_size
+            cell_size_y_abs = config.img_height/config.grid_size
+            # center coordinates relative to grid cell
+            # e.g. x_center_rel = (584 - 3.5 * 183) / 183 = -0.3 (left of center of cell 3)
+            x_center_rel = (x_center_abs - (i+0.5) * cell_size_x_abs ) / cell_size_x_abs
+            y_center_rel = (y_center_abs - (j+0.5) * cell_size_y_abs ) / cell_size_y_abs
+            # width/height coordinates relative to grid cell
+            # e.g. width_rel = 50 / 1280 * 7 = 0.27 grid cells
+            width_rel = bbox["width"] / config.img_width * config.grid_size
+            height_rel = bbox["height"] / config.img_height * config.grid_size
+            # write [objectness, center_x, center_y, width, height] into bbox grid cell
+            y[i,j] = [1, x_center_rel, y_center_rel, width_rel, height_rel]
+        return y
     
-    # |    train      |         test  |
-    # |    80%        |         20%   |
-    #        len_train_ds          
+    def load(self):
+    
 
-    text_ds_train = tf.data.Dataset.from_tensor_slices((img_names[:len_train_ds], y[:len_train_ds]))
-    text_ds_test = tf.data.Dataset.from_tensor_slices((img_names[len_train_ds:], y[len_train_ds:]))
+    
+
+    def convert_label_to_y(label, file):
+        object_class, x_center, y_center, width_bbox, height_bbox = list(map(float, label))
+
+        # find relevant grid cell for predicting the object
+        i, j = int(y_center * config.grid_size), int(x_center * config.grid_size) # i row, j column 
+
+        assert i < config.grid_size, "center must be within the image" + str(file.name)
+        assert j < config.grid_size, "center must be within the image" + str(file.name)
+
+        x_center_bbox = x_center * config.grid_size - j  # find center relative to cell; 0...1
+        y_center_bbox = y_center * config.grid_size - i
+
+        width_bbox *= config.grid_size  # scale box size, that 1 equals the grid size -> range of object size makes more sense
+        height_bbox *= config.grid_size
+
+        c0, c1, c2, c3, c4, c5, c6 = tf.one_hot(tf.range(7), 7)[int(object_class)] # class of the cone
+        # objectness, x_center, y_center, width_bbox, height_bbox, class0, c1, c2, c3, c4, c5, c6
+        y = [1.0, x_center_bbox, y_center_bbox, width_bbox, height_bbox, c0, c1, c2, c3, c4, c5, c6]
+        return y, i, j
+
+
+    def read_data(path, black_list_files=None):
+        if black_list_files is None:
+            black_list_files = []
+        jpg_paths = []
+        n_samples = 0
+        for file in os.listdir(path):
+            if file.endswith(".txt"):
+                n_samples += 1
+        n_samples -= len(black_list_files)
+
+        sample_ctr = 0
+        y = np.zeros((n_samples, 7, 7, 12))  # obj + bbox + classes = 1 + 4 + 7 = 12
+        for file in os.listdir(path):
+            if file.endswith(".txt"):
+            
+                if file in black_list_files:
+                    continue
+                #try:
+                with open(os.path.join(path, file), "r") as file:
+                    for label in file:
+                        label = label.strip().split(" ")
+                        cone, i, j = convert_label_to_y(label, file) 
+                        
+                        if y[sample_ctr, j, i, 0] == 0:
+                            # no cone is stored at the grid cell, yet
+                            y[sample_ctr, j, i, :] = cone
+                        # else:
+                            # slot already full- don´t write the cone
+                            # print("No space to store the cone. Cone will be dismissed. File: ", file.name, i,j)
+                            # think about dismissing the whole image and don´t use it for training
+                            
+                #except:
+                #    print(str(file))
+                #    continue
+                #find the corresponding image path
+                jpg_path = file.name.strip("txt")+"jpg"
+                jpg_paths.append(jpg_path)
+                sample_ctr += 1
+                
+        return jpg_paths, y
+    ds_train = tf.data.Dataset.from_tensor_slices((jpg_paths_train, y_train))
+    ds_test = tf.data.Dataset.from_tensor_slices((jpg_paths_test, y_test))
+
+    def read_image(image_file, y):
+        image = tf.io.read_file(image_file)
+        image = tf.image.decode_jpeg(image, channels=3)#, dtype=tf.float32)
+        return image, y
+
+    def resize(image, y):
+        image = tf.image.resize(image, [img_height, img_width], method=tf.image.ResizeMethod.BILINEAR,preserve_aspect_ratio=False)
+        # skip rescaling, because resnet expects int8
+        #image = image / 255. # rescale
+        return image, y
+
+
+    #ds_train = ds_train.map(read_image).map(resize_and_crop).map(augment).batch(batch_size)
+    ds_train = ds_train.map(read_image).map(resize).shuffle(train_size).batch(batch_size).prefetch(2)
+    ds_test = ds_test.map(read_image).map(resize).batch(batch_size)
+
+
 
 
     def img_name_to_image(img_names, y):
@@ -85,43 +209,7 @@ def load(config):
     return ds_train, ds_train_not_shuffled, ds_test
 
 
-def bbox_to_grid(config, bboxes):
-    """
-    read csv entry for bbox and transform it to matrices for x, y, width and height
-    e.g. {"x": 559, "y": 213, "width":50, "height": 32} to 7x7x5 tensor (including objectness)
-    return: y
-    """
-    y = np.zeros((config.grid_size, config.grid_size, 5))
 
-    bboxes = ast.literal_eval(bboxes)
-    # calculate grid positions for bbox centers
-    for bbox in bboxes:
-        # absolute coordinates for center 
-        # e.g. x_abs = position + half the width 
-        x_center_abs = bbox["x"] + int(bbox["width"] / 2)
-        y_center_abs = bbox["y"] + int(bbox["height"] / 2)
-        # to which grid cell do those coordinates belong
-        # e.g. x_center_abs: 584 to cell 3
-        i = int(x_center_abs / config.img_width * config.grid_size) # grid cell x
-        j = int(y_center_abs / config.img_height * config.grid_size) # grid cell y
-        if i == config.grid_size:
-            i = i-1
-        if j == config.grid_size:
-            j = j-1
-        # absolute width/height per cell
-        cell_size_x_abs = config.img_width/config.grid_size
-        cell_size_y_abs = config.img_height/config.grid_size
-        # center coordinates relative to grid cell
-        # e.g. x_center_rel = (584 - 3.5 * 183) / 183 = -0.3 (left of center of cell 3)
-        x_center_rel = (x_center_abs - (i+0.5) * cell_size_x_abs ) / cell_size_x_abs
-        y_center_rel = (y_center_abs - (j+0.5) * cell_size_y_abs ) / cell_size_y_abs
-        # width/height coordinates relative to grid cell
-        # e.g. width_rel = 50 / 1280 * 7 = 0.27 grid cells
-        width_rel = bbox["width"] / config.img_width * config.grid_size
-        height_rel = bbox["height"] / config.img_height * config.grid_size
-        # write [objectness, center_x, center_y, width, height] into bbox grid cell
-        y[i,j] = [1, x_center_rel, y_center_rel, width_rel, height_rel]
-    return y
 
 
 def grid_to_bboxes(config, grid, color="white"):
@@ -191,6 +279,16 @@ if __name__ == "__main__":
     config = wandb.config
 
     print("Load dataset")
+
+    
+    jpg_paths_train, y_train = read_data(config.data_dir + "train/") #, black_list_files_train)
+    jpg_paths_test, y_test = read_data(config.data_dir + "test/")  
+
+    print(y_train.shape[0], " train images found.")
+    print(y_test.shape[0], " test images found.")
+
+
+    """
     ds_train, _, ds_test = load(config)
 
 
@@ -204,7 +302,7 @@ if __name__ == "__main__":
  
         plt.imshow(img)
         plt.show()
- 
+    """
         
 
         
