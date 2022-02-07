ad01bdae58e73b5a9b59b507664e90c10d2ec35e
st172353 <st172353>
2022-02-07 16:49:30 +0100

    clean up architecture

--- Traffic_cone_detection/architecture.py
+++ Traffic_cone_detection/architecture.py
@@ -5,22 +5,13 @@ import numpy as np
 
 
 def transfer_model(config): 
-    # select transfer model type
-    if config.architecture == "vgg16":
-        base_model = keras.applications.VGG16(
-            weights='imagenet',
-            include_top=False,
-            input_shape=config.cnn_input_shape)
-        preprocess_input = keras.applications.vgg16.preprocess_input
-    elif config.architecture == "resnet50":
-        base_model = keras.applications.ResNet50(
-            weights='imagenet',
-            include_top=False,
-            input_shape=config.cnn_input_shape)
-        preprocess_input = keras.applications.resnet50.preprocess_input
-    else:
-        print(f"{config.architecture} model not defined!")
+    # use Resnet50 as transfer learning model
+    base_model = keras.applications.ResNet50(weights='imagenet',
+                                        include_top=False,
+                                        input_shape=config.cnn_input_shape)
+    preprocess_input = keras.applications.resnet50.preprocess_input
 
+    # selectable via config which layers are trainable
     if config.trainable == "full":
         for layer in base_model.layers:
             layer.trainable = True
@@ -31,46 +22,55 @@ def transfer_model(config):
         for layer in base_model.layers:
             layer.trainable = False
             
-    #base_model.summary()
     inputs = keras.layers.Input(shape=config.cnn_input_shape, dtype=tf.uint8)
     x = tf.cast(inputs, tf.float32)
     x = preprocess_input(x)
     x = base_model(x)
     
-    # 14x14x2048
-    x = keras.layers.Conv2D(512, (3,3), strides=(1,1), padding="same", activation=tf.keras.layers.LeakyReLU(alpha=0.01))(x)
-    #x = keras.layers.Dropout(config.dropout)(x)
-    x = keras.layers.Conv2D(256, (1,1), strides=(1,1), padding="same", activation=tf.keras.layers.LeakyReLU(alpha=0.01))(x)
-    #x = keras.layers.Dropout(config.dropout)(x)
-    x = keras.layers.Conv2D(256, (3,3), strides=(1,1), padding="same", activation=tf.keras.layers.LeakyReLU(alpha=0.01))(x)
-    #x = keras.layers.Dropout(config.dropout)(x)
-    x = keras.layers.Conv2D(128, (1,1), strides=(1,1), padding="same", activation=tf.keras.layers.LeakyReLU(alpha=0.01))(x)
-    #x = keras.layers.Dropout(config.dropout)(x)
-    x = keras.layers.Conv2D(128, (3,3), strides=(1,1), padding="same", activation=tf.keras.layers.LeakyReLU(alpha=0.01))(x)
-    #x = keras.layers.Dropout(config.dropout)(x)
-    #x = keras.layers.MaxPool2D(pool_size=(2,2))(x)
+    # output of resnet50: 14x14x2048
 
-    #x = keras.layers.Conv2D(256, (3,3), strides=(1,1), padding="same", activation=tf.keras.layers.LeakyReLU(alpha=0.01))(x)
-    #x = keras.layers.Dropout(config.dropout)(x)
-    #x = keras.layers.Conv2D(128, (1,1), strides=(1,1), padding="same", activation=tf.keras.layers.LeakyReLU(alpha=0.01))(x)
-    #x = keras.layers.Dropout(config.dropout)(x)
-    #x = keras.layers.Conv2D(64, (3,3), strides=(1,1), padding="same", activation=tf.keras.layers.LeakyReLU(alpha=0.01))(x)
-    x = keras.layers.Dropout(config.dropout)(x)
-    outputs = keras.layers.Conv2D(5, (1,1), strides=(1,1), padding="same", activation="linear")(x)
-    # 7x7x5 
+    if config.grid_size == 7:
+        # if grid size = 7, MaxPooling is required, select a deeper architecture
+        # switch between 3x3 conv and 1x1 conv as in YOLO9000 
+        x = keras.layers.Conv2D(512, (3,3), strides=(1,1), padding="same", activation=tf.keras.layers.LeakyReLU(alpha=0.01))(x)
+        x = keras.layers.Conv2D(256, (1,1), strides=(1,1), padding="same", activation=tf.keras.layers.LeakyReLU(alpha=0.01))(x)
+        x = keras.layers.Conv2D(256, (3,3), strides=(1,1), padding="same", activation=tf.keras.layers.LeakyReLU(alpha=0.01))(x)
+        x = keras.layers.Conv2D(128, (1,1), strides=(1,1), padding="same", activation=tf.keras.layers.LeakyReLU(alpha=0.01))(x)
+        x = keras.layers.Conv2D(128, (3,3), strides=(1,1), padding="same", activation=tf.keras.layers.LeakyReLU(alpha=0.01))(x)
+        x = keras.layers.MaxPool2D(pool_size=(2,2))(x)
+
+        #x = keras.layers.Conv2D(256, (3,3), strides=(1,1), padding="same", activation=tf.keras.layers.LeakyReLU(alpha=0.01))(x)
+        #x = keras.layers.Dropout(config.dropout)(x)
+        #x = keras.layers.Conv2D(128, (1,1), strides=(1,1), padding="same", activation=tf.keras.layers.LeakyReLU(alpha=0.01))(x)
+        #x = keras.layers.Dropout(config.dropout)(x)
+        #x = keras.layers.Conv2D(64, (3,3), strides=(1,1), padding="same", activation=tf.keras.layers.LeakyReLU(alpha=0.01))(x)
+        x = keras.layers.Dropout(config.dropout)(x)
+        outputs = keras.layers.Conv2D(5, (1,1), strides=(1,1), padding="same", activation="linear")(x)
+        # 7x7x5 
  
-    
+    elif config.grid_size == 14:
+        # if grid size = 14, no pooling is requried
+        #x = keras.layers.Conv2D(512, (3,3), strides=(1,1), padding="same", activation=tf.keras.layers.LeakyReLU(alpha=0.01))(x)
+        #x = keras.layers.Conv2D(256, (1,1), strides=(1,1), padding="same", activation=tf.keras.layers.LeakyReLU(alpha=0.01))(x)
+        #x = keras.layers.Conv2D(256, (3,3), strides=(1,1), padding="same", activation=tf.keras.layers.LeakyReLU(alpha=0.01))(x)
+        #x = keras.layers.Conv2D(128, (1,1), strides=(1,1), padding="same", activation=tf.keras.layers.LeakyReLU(alpha=0.01))(x)
+        #x = keras.layers.Conv2D(128, (3,3), strides=(1,1), padding="same", activation=tf.keras.layers.LeakyReLU(alpha=0.01))(x)
+        x = keras.layers.Dropout(config.dropout)(x)
+        outputs = keras.layers.Conv2D(5, (1,1), strides=(1,1), padding="same", activation="linear")(x)
+        # 7x7x5 
+
+    else:
+        print("Other grid size than 7 or 14 is not supported by architecture.py")
 
     return keras.Model(inputs=inputs, outputs=outputs, name="yolo")
 
 
 if __name__ == "__main__":
     import wandb
-    #from tensorflow.python.framework.ops import disable_eager_execution
-    #disable_eager_execution()
 
     wandb.init(project="test", entity="team8", mode="disabled") 
     config = wandb.config
     
+    # load the model and print the summary
     model = transfer_model(config)
     model.summary()
